32d1dc7504a000d7bcad2df41cf67f8de05d501e
Jensel Gatchalian <jensel.gatchalian@gmail.com>
2016-03-24 10:54:22 -0700

    init repo

---
+++ custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+  $(".fa-git-square").mouseover(function(){
+    $(".fa-git-square").css("color","#E64A19");
+  });
+
+  $(".fa-facebook-square").mouseover(function(){
+    $(".fa-facebook-square").css("color","#E64A19");
+  });
+
+  $(".fa-linkedin-square").mouseover(function(){
+    $(".fa-linkedin-square").css("color","#E64A19");
+  });
+
+  $(".fa-twitter-square").mouseover(function(){
+    $(".fa-twitter-square").css("color","#E64A19");
+  });
+
+  $(".fa-google-plus-square").mouseover(function(){
+    $(".fa-google-plus-square").css("color","#E64A19");
+  });
+
+  $(".fa-reddit-square").mouseover(function(){
+    $(".fa-reddit-square").css("color","#E64A19");
+  });
+
+  $(".fa").mouseout(function(){
+    $(".fa").css("color","inherit");
+  });
+
+});
